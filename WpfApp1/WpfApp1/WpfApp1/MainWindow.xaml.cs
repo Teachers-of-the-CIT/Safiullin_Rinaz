8ac2c583d283cedfc7916566c7da44acd7739bf2
Rinaz4338 <93836975+Rinaz4338@users.noreply.github.com>
2022-11-05 12:15:23 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logwinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.RegWindow regWindow = new Windows.RegWindow();
+            this.Hide();
+            regWindow.Show();
+        }
+
+        private void passTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void submitBTN_Click(object sender, RoutedEventArgs e)
+        {
+            using (Models.ParfumeriaEntities db = new Models.ParfumeriaEntities())
+            {
+                var user = db.Users.FirstOrDefault(x => x.login == logTB.Text &&
+                                                      x.password == passTB.Text);
+                if (user != null)
+                {
+
+                    Windows.SpisokTovarovWindow stWindow = new Windows.SpisokTovarovWindow();
+                    stWindow.Show();
+                    return;
+                }
+                MessageBox.Show("Пройдите регистрацию!");
+                Windows.SpisokTovarovWindow spisokTovarovWindow = new Windows.SpisokTovarovWindow();
+                this.Hide();
+                spisokTovarovWindow.Show();
+
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/WpfApp1/Windows/RegWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegWindow.xaml
+    /// </summary>
+    public partial class RegWindow : Window
+    {
+        public RegWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void autwinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Hide();
+            mainWindow.Show();
+
+        }
+
+
+
+        private void regBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string role = "";
+            if (logregTB.Text == "" && passregTB.Text == "")
+            {
+                MessageBox.Show("Поля не должны быть пустыми.");
+                return;
+            }
+            using (Models.ParfumeriaEntities db = new Models.ParfumeriaEntities())
+            {
+                var user = db.Users.FirstOrDefault(x => x.login == logregTB.Text &&
+                                                      x.password == passregTB.Text);
+                if (user != null)
+                {
+                    MessageBox.Show("Вы уже есть в моей базе, пройдите авторизацию!");
+                    return;
+                }
+                if (logregTB.Text == "Admin")
+                {
+                    role = "Admin";
+                }
+                if (logregTB.Text == "Manager")
+                {
+                    role = "Manager";
+                }
+                if (logregTB.Text == "User")
+                {
+                    role = "User";
+                }
+                db.Users.Add(
+                    new Models.Users() { login = logregTB.Text, password = passregTB.Text, role = role });
+                db.SaveChanges();
+                MessageBox.Show("Регистрация успешна!");
+                Windows.SpisokTovarovWindow spisokTovarovWindow = new Windows.SpisokTovarovWindow();
+                this.Hide();
+                spisokTovarovWindow.Show();
+            }
+        }
+    }
+}
